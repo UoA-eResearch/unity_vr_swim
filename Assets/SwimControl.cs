7aa4564714cbb7ecc5d641587f3d31276dd65940
Nick Young <nick.young@auckland.ac.nz>
2019-10-07 17:30:40 +1300

    add SwimControl script

---
+++ Assets/SwimControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SwimControl : MonoBehaviour
+{
+    public int swimForceMultiplier = 100;
+    private SteamVR_Action_Pose poseAction;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poseAction = SteamVR_Input.GetAction<SteamVR_Action_Pose>("Pose");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var combined_velocity = poseAction[SteamVR_Input_Sources.LeftHand].velocity.magnitude + poseAction[SteamVR_Input_Sources.RightHand].velocity.magnitude;
+        Debug.Log("vel: " + combined_velocity);
+        rb.AddForce(Camera.main.transform.forward * combined_velocity * swimForceMultiplier);
+    }
+}
