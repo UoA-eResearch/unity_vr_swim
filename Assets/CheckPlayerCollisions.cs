bc1ccbb22e58d5bd2c8f9f00a6b88bb949440696
Nick Young <nick.young@auckland.ac.nz>
2019-10-23 15:47:48 +1300

    add 3D model of Piha. Add scene for Piha. Setup win/loss states

---
+++ Assets/CheckPlayerCollisions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPlayerCollisions : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float ripForce = 100f;
+    public GameObject deathText;
+    public GameObject winText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Collision with " + collision.name);
+        if (collision.name == "DeathZone")
+        {
+            deathText.SetActive(true);
+            winText.SetActive(false);
+        } else if (collision.name == "WinZone")
+        {
+            winText.SetActive(true);
+            deathText.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.tag == "ForceField")
+        {
+            rb.AddForce(collision.transform.forward * ripForce);
+        }
+    }
+}
--- Assets/SwimControl.cs
+++ Assets/SwimControl.cs
@@ -8,6 +8,7 @@ public class SwimControl : MonoBehaviour
     public int swimForceMultiplier = 100;
     private SteamVR_Action_Pose poseAction;
     private Rigidbody rb;
+    public bool logVelocity = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +21,10 @@ public class SwimControl : MonoBehaviour
     void Update()
     {
         var combined_velocity = poseAction[SteamVR_Input_Sources.LeftHand].velocity.magnitude + poseAction[SteamVR_Input_Sources.RightHand].velocity.magnitude;
-        Debug.Log("vel: " + combined_velocity);
+        if (logVelocity)
+        {
+            Debug.Log("vel: " + combined_velocity);
+        }
         rb.AddForce(Camera.main.transform.forward * combined_velocity * swimForceMultiplier);
     }
 }
